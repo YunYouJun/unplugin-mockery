43862dc006874b994fa48fd8c85cc99080910ef4
YunYouJun <me@yunyoujun.cn>
2024-07-26 15:29:27 +0800

    feat: add client template

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# unplugin-mockery/client
---
+++ client/src/layouts/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <main
+    px-4 py-10
+    text="center gray-700 dark:gray-200"
+  >
+    <RouterView />
+    <!-- <TheFooter /> -->
+  </main>
+</template>
