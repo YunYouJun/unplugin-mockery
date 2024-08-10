cc5e9aca23330fa71188087ae28b88b9587df5a5
YunYouJun <me@yunyoujun.cn>
2024-08-10 17:02:04 +0800

    chore: remove milkio

--- milkio/src/router.ts
+++
@@ -1,7 +0,0 @@
-import type ApiParams from '../generated/api-schema'
-
-// eslint-disable-next-line unused-imports/no-unused-vars
-export async function routerHandler(path: string, fullurl: URL): Promise<false | keyof (typeof ApiParams)['apiMethodsSchema']> {
-  // ...
-  return false
-}
